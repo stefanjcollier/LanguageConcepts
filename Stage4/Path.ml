d7e1e632e270f29da0c7a88a48972810703470d9
Matt DaSilva <matt_das@hotmail.co.uk>
2015-03-03 17:06:43 +0000

    LANG CONCAT FUCK YEAH

---
+++ Stage4/Path.ml
@@ -0,0 +1,149 @@
+module WordSet = Set.Make(String);;
+let lang_hash = Hashtbl.create 100;;
+let word_hash = Hashtbl.create 100;;
+let int_hash = Hashtbl.create 100;;
+
+type word = 
+ | WordLiteral of string
+ | WordVarialbe of string
+ | WordConcat of (word * word)
+;;
+
+type lang_literal =
+ | Empty_Set
+ | Cons of (word * lang_literal)
+;;
+
+type language =
+ | LangLiteral of lang_literal
+ | LangVariable of string
+ | Union of (language * language)
+ | Intersection of (language * language)
+ | Subtraction of (language * language)
+ | LangConcat of (language * language)
+;;
+
+type integer = 
+| IntLiteral of int
+| IntVariable of string
+
+type decs = 
+ | IntDeclaration of (string * int)
+ | WordDeclaration of (string * word)
+ | LangDeclaration of (string * language)
+;;
+
+type program =
+ | Output of language
+ | Declaration of decs
+ | Statement of language
+ | WStatement of word
+ | Statements of (program * program)
+;;
+
+
+let rec solve_word = function
+ | WordLiteral(str) -> str
+ | WordVarialbe(var) -> Hashtbl.find word_hash var
+ | WordConcat(w1, w2) -> (solve_word w1)^(solve_word w2)
+;;
+
+ (* Converts the data type into a set *)
+let rec convert_literal_lang = function
+ | Empty_Set -> WordSet.empty 
+ | Cons (word, more) -> WordSet.add (solve_word word) ( convert_literal_lang more )
+;;
+ 
+(*
+let rec yolo w1 w2 =
+	print_string (w1^w2)
+;;
+
+let rec somthiung s w =
+	WordSet.iter (yolo w) s
+;;
+let lang_concat l1 l2 =
+	let rec loop2 wrd1 wrd2 result =
+		result = WordSet.add result wrd1^wrd2
+	in
+	let rec loop1 set wrd result=
+	WordSet.iter (loop2 wrd) set 
+in
+	WordSet.iter (loop1 l2) l1
+;;
+*)
+
+let rec addWord (lst:string list) (wrd:string) =
+ 	match lst with
+	| [] -> []
+	| [x] -> [x^wrd]
+	| h::t -> (h^wrd)  ::  addWord t wrd
+;; 
+
+let rec list_concat l1 l2 =
+	match l1,l2 with
+	| [],_ -> []
+	| _,[] -> []
+	| [x],_ -> addWord l2 x
+	| h::t,_ -> List.append (addWord l2 h) (list_concat t l2) 
+;;
+
+let lang_concat s1 s2 =
+	let res = list_concat (WordSet.elements s2) (WordSet.elements s1) in
+	List.fold_right WordSet.add res WordSet.empty
+;;
+
+
+(* Retrieves the set ascociated with the variable or derived from the literal 
+   @Returns a SET*)
+let rec solve_lang = function
+ | LangLiteral(lit) -> 		convert_literal_lang lit
+ | LangVariable(var) -> 	Hashtbl.find lang_hash var
+ | Union (l1, l2) -> 		WordSet.union (solve_lang l1) (solve_lang l2)
+ | Intersection (l1, l2) -> WordSet.inter (solve_lang l1) (solve_lang l2) 
+ | Subtraction (l1, l2) -> 	WordSet.diff  (solve_lang l1) (solve_lang l2)
+ | LangConcat (l1, l2) ->  lang_concat (solve_lang l1) (solve_lang l2)
+;;
+
+let redeclare = function
+ | LangDeclaration(str , vl) -> Hashtbl.replace lang_hash str (solve_lang vl)
+ | WordDeclaration(str, vl) -> Hashtbl.replace  word_hash str  (solve_word vl)
+ | IntDeclaration(str, vl) -> Hashtbl.replace   int_hash  str  vl
+;;
+
+let language_of_string lang = 
+	let rec aux (lst:string list) = 
+	match lst with
+ 		| [] -> "}"
+ 		| [x] -> x^"}"
+ 		| h::t -> h^", "^(aux t)
+	 in
+	 "{"^aux (WordSet.elements (solve_lang lang))^"\n" 
+;;
+(* Takes a WordSet obj and print_string's a set *)
+let print_language lang =
+	 print_string (language_of_string lang)
+;;
+
+
+let print_language_max max lang =
+	let rec aux (lst:string list) accum = 
+	match lst with
+ 		| [] -> "}"
+ 		| [x] -> (x^"}")
+ 		| h::t -> if accum+1 = max 
+ 				then h^"}" 
+ 				else h^", "^aux t (accum+1)
+	 in
+	 print_string (("{"^aux(WordSet.elements (solve_lang lang)) 0)^"\n")
+;;
+
+let rec interpret (prog:program) =
+	match prog with
+	| Output (lang)-> (print_language lang); 
+	| Declaration(decObj) -> redeclare decObj;
+	| Statement( lang )-> print_string ("unassigned lang: "^(language_of_string lang)); 
+	| WStatement( word )-> print_string ("unassigned word:"^(solve_word word)^"\n");
+	| Statements ( head, tail ) -> interpret head; interpret tail;
+;;
+
---
+++ Stage4/love.ml
@@ -0,0 +1,15 @@
+(* File love.ml *)
+open Path
+
+let _ =
+try
+  let lexbuf = Lexing.from_channel stdin in
+   while true do
+     let result = Parser.main Lexer.token lexbuf in
+       interpret result; print_newline(); flush stdout
+   done
+with Lexer.Eof ->
+print_string "Adios Mother fucker" ;
+	exit 0
+
+
